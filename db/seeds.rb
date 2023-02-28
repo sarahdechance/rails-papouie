dff227bc33d4f2ff8306fa087b21cf9e69d811ed
sarah dechance <sarah.dechance@gmail.com>
2023-02-28 14:39:15 +0100

    new user button and seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,38 +9,41 @@
 # puts "Deleting User data"
 # User.destroy_all
 
-# puts "Generating User data"
+puts "Generating User data"
 
-# 10.times do
-#   first_name = Faker::Name.first_name
-#   last_name = Faker::Name.last_name
-#   pseudo = "#{first_name}.#{last_name}"
-#   email = "#{pseudo}@gmail.com"
-#   user = User.new({first_name: first_name, last_name: last_name, pseudo: pseudo, email: email})
-#   user.save
-#   puts user
-# end
+4.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  pseudo = "#{first_name}.#{last_name}"
+  email = "#{pseudo}@gmail.com"
+  password = "azerty"
+  user = User.new({first_name: first_name, last_name: last_name, pseudo: pseudo, password: password, email: email})
+  user.save
+  puts user
+end
 
-# puts "User data generated"
+puts "User data generated"
 
-# puts "-"*20
+puts "-"*20
 
-# puts "Deleting Offer data"
-# Offer.destroy_all
+puts "Deleting Offer data"
+Offer.destroy_all
 
-# puts "Generating User data"
-# 2.times do
-#   users = User.all
-#   users.each do |user|
-#     hourly_price = (20..200).sample
-#     longitude = (-180..180).sample
-#     latitude = (-90..90).sample
-#     description = Faker::Quote.famous_last_words
-#     user_id = user.id
-#     offer = Offer.new({hourly_price: hourly_price, longitude: longitude, latitude: latitude, description: description, user_id: user_id})
-#     offer.save
-#     puts offer
-#   end
-# end
+puts "Generating User data"
+2.times do
+  users = User.all
+  users.each do |user|
+    name = "Papouilles de #{user.first_name}"
+    hourly_price = rand(20..200)
+    longitude = rand(-180..180)
+    latitude = rand(-90..90)
+    description = Faker::Quote.famous_last_words
+    user_id = user.id
+    offer = Offer.new({name: name, hourly_price: hourly_price, longitude: longitude, latitude: latitude, description: description, user_id: user_id})
+    puts offer.valid?
+    puts offer.errors.messages
+    offer.save
+  end
+end
 
-# puts "Offer data generated"
+puts "Offer data generated"
