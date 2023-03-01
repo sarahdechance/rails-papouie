783c293361e67d7a7449f0625fd3339c09feef6e
sarah dechance <sarah.dechance@gmail.com>
2023-03-01 18:17:02 +0100

    new offer form functioning

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_114557) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_165025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_114557) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.boolean "availability"
+    t.string "address"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
